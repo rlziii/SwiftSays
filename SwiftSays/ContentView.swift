00dec7d0fe95edeea3488fd3d349eefa95f71251
Richard L Zarth III <rlziii@icloud.com>
2022-04-12 21:08:43 -0400

    Initial commit.

---
+++ SwiftSays/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftSays
+//
+//  Created by Richard L Zarth III on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftSays/SwiftSaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSaysApp.swift
+//  SwiftSays
+//
+//  Created by Richard L Zarth III on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
